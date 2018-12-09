b5acd20b98a5d1301e795d4968ac233be9d76b60
TacoTheDank <SkytkRSfan3895@gmail.com>
2018-12-09 11:39:42 -0500

    update several packages and links, and fix some misspellings of "maintainer"
    Signed-off-by: TacoTheDank <SkytkRSfan3895@gmail.com>

--- build-root-package.sh
+++ build-root-package.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -u
 
 # link into main package repo to handle dependencies
 for root_package in packages/*; do
-    ln -sf ../../$root_package termux-packages/packages/
+    ln -sf ../../${root_package} termux-packages/packages/
 done
 
 cd termux-packages
