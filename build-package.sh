b3b367257f41c2b409d51b02e704c642837818ab
Henrik Grimler <henrik@grimler.se>
2019-01-25 22:05:57 +0100

    rename build wrapper

--- build-package.sh
+++ build-package.sh
@@ -3,8 +3,9 @@
 set -e -o pipefail -u
 
 # link into main package repo to handle dependencies
-for root_package in packages/*; do
-    ln -sf ../../${root_package} termux-packages/packages/
+for package in packages/*; do
+    #TODO: handle docker image builds better
+    ln -sf ../../${package} termux-packages/packages/
 done
 
 cd termux-packages
