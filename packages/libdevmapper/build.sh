b206ef32dca83a6d4662b73f5789589a7f7ad262
Henrik Grimler <henrik@grimler.se>
2019-02-09 18:25:52 +0100

    libdevmapper: update to 2.03.02

--- packages/libdevmapper/build.sh
+++ packages/libdevmapper/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://sourceware.org/lvm2/
 TERMUX_PKG_DESCRIPTION="A device-mapper library from LVM2 package"
 TERMUX_PKG_LICENSE="GPL-2.0"
-# REAL VERSION: TERMUX_PKG_VERSION=2.02.177
-TERMUX_PKG_VERSION=2.03.01
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/sourceware/lvm2/releases/LVM2.2.02.177.tgz
-TERMUX_PKG_SHA256=4025a23ec9b15c2cb7486d151c29dc953b75efc4d452cfe9dbbc7c0fac8e80f2
+TERMUX_PKG_VERSION=2.03.02
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/sourceware/lvm2/releases/LVM2.${TERMUX_PKG_VERSION}.tgz
+TERMUX_PKG_SHA256=550ba750239fd75b7e52c9877565cabffef506bbf6d7f6f17b9700dee56c720f
 TERMUX_PKG_DEPENDS="libandroid-support, libaio, readline"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_API_LEVEL=23
@@ -17,4 +16,3 @@ termux_step_make_install() {
 	cd libdm
 	make install
 }
-
