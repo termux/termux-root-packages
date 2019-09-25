1f54e62365f450c482f070de8bd0a4b0e5f2bb76
Henrik Grimler <henrik@grimler.se>
2019-09-25 10:12:19 +0200

    Update CI scripts to latest versions

--- scripts/cirrus-ci_dispatcher.sh
+++ scripts/cirrus-ci_dispatcher.sh
@@ -5,11 +5,16 @@
 
 set -e
 
+## Some packages should be excluded from auto builds.
+EXCLUDED_PACKAGES="lldb rust texlive"
+
 ###############################################################################
 ##
-##  Preparation.
+##  Determining changes.
 ##
 ###############################################################################
+
+set +e
 
 REPO_DIR=$(realpath "$(dirname "$(realpath "$0")")/../")
 cd "$REPO_DIR" || {
@@ -24,7 +29,12 @@ if [ $# -ge 1 ]; then
 	fi
 fi
 
-BUILD_ENVIRONMENT="termux-packages"
+# Some environment variables are important for correct functionality
+# of this script.
+if [ -z "$CIRRUS_CHANGE_IN_REPO" ]; then
+	echo "[!] CIRRUS_CHANGE_IN_REPO is not set."
+	exit 1
+fi
 
 if ! $DO_UPLOAD; then
 	echo "[*] Initializing submodules:"
@@ -36,9 +46,9 @@ if ! $DO_UPLOAD; then
 	echo
 	for pkg in "${REPO_DIR}"/packages/*; do
 		if [ -d "$pkg" ]; then
-			if [ ! -e "${REPO_DIR}/${BUILD_ENVIRONMENT}/packages/$(basename "$pkg")" ]; then
+			if [ ! -e "${REPO_DIR}/termux-packages/packages/$(basename "$pkg")" ]; then
 				echo "    - $(basename "$pkg")"
-				cp -a "$pkg" "${REPO_DIR}/${BUILD_ENVIRONMENT}"/packages/
+				cp -a "$pkg" "${REPO_DIR}/termux-packages/packages/"
 			else
 				echo "    - $(basename "$pkg"): package already exist, skipping"
 			fi
@@ -47,132 +57,102 @@ if ! $DO_UPLOAD; then
 	echo
 fi
 
-###############################################################################
-##
-##  Determining changes.
-##
-###############################################################################
-
-set +e
+if [ -n "$CIRRUS_PR" ] && [ -z "$CIRRUS_BASE_SHA" ]; then
+	echo "[!] CIRRUS_BASE_SHA is not set."
+	exit 1
+fi
 
 # Process tag '%ci:no-build' that may be added as line to commit message.
 # Will force CI to exit with status 'passed' without performing build.
 if grep -qiP '^\s*%ci:no-build\s*$' <(git log --format="%B" -n 1 "$CIRRUS_CHANGE_IN_REPO"); then
-	echo "[*] Exiting with status 'passed' (tag '%ci:no-build' applied)."
+	echo "[!] Exiting with status 'passed' (tag '%ci:no-build' applied)."
 	exit 0
 fi
 
 # Process tag '%ci:reset-backlog' that may be added as line to commit message.
 # Will force CI to build changes only for the current commit.
 if grep -qiP '^\s*%ci:reset-backlog\s*$' <(git log --format="%B" -n 1 "$CIRRUS_CHANGE_IN_REPO"); then
-	echo "[*] Building only last pushed commit (tag '%ci:reset-backlog' applied)."
+	echo "[!] Building only last pushed commit (tag '%ci:reset-backlog' applied)."
 	unset CIRRUS_LAST_GREEN_CHANGE
 	unset CIRRUS_BASE_SHA
 fi
 
 if [ -z "$CIRRUS_PR" ]; then
+	# Changes determined from the last commit where CI finished with status
+	# 'passed' (green) and the top commit.
 	if [ -z "$CIRRUS_LAST_GREEN_CHANGE" ]; then
-		UPDATED_FILES=$(git diff-tree --no-commit-id --name-only -r "$CIRRUS_CHANGE_IN_REPO" 2>/dev/null | grep -P "packages/")
+		GIT_CHANGES="$CIRRUS_CHANGE_IN_REPO"
 	else
-		UPDATED_FILES=$(git diff-tree --no-commit-id --name-only -r "${CIRRUS_LAST_GREEN_CHANGE}..${CIRRUS_CHANGE_IN_REPO}" 2>/dev/null | grep -P "packages/")
+		GIT_CHANGES="${CIRRUS_LAST_GREEN_CHANGE}..${CIRRUS_CHANGE_IN_REPO}"
 	fi
+	echo "[*] Changes: $GIT_CHANGES"
 else
-	# Pull requests are handled in a bit different way.
-	UPDATED_FILES=$(git diff-tree --no-commit-id --name-only -r "${CIRRUS_BASE_SHA}..${CIRRUS_CHANGE_IN_REPO}" 2>/dev/null | grep -P "packages/")
+	# Changes in pull request are determined from commits between the
+	# top commit of base branch and latest commit of PR's branch.
+	GIT_CHANGES="${CIRRUS_BASE_SHA}..${CIRRUS_CHANGE_IN_REPO}"
+	echo "[*] Pull request: https://github.com/termux/termux-root-packages/pull/${CIRRUS_PR}"
 fi
 
-## Determine modified packages.
-existing_dirs=""
-for dir in $(echo "$UPDATED_FILES" | grep -oP "packages/[a-z0-9+._-]+" | sort | uniq); do
-	if [ -d "${REPO_DIR}/${dir}" ]; then
-		existing_dirs+=" $dir"
+# Determine changes from commit range.
+CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r "$GIT_CHANGES" 2>/dev/null)
+
+# Modified packages.
+PACKAGE_NAMES=$(sed -nE 's@^packages/([^/]*)/build.sh@\1@p' <<< "$CHANGED_FILES")
+
+unset CHANGED_FILES
+
+## Filter deleted packages.
+for pkg in $PACKAGE_NAMES; do
+	if [ ! -d "${REPO_DIR}/packages/${pkg}" ]; then
+		PACKAGE_NAMES=$(sed -E "s/(^|\s\s*)${pkg}(\$|\s\s*)/ /g" <<< "$PACKAGE_NAMES")
 	fi
 done
-PACKAGE_DIRS="$existing_dirs"
-unset dir existing_dirs
 
-## Get names of modified packages.
-PACKAGE_NAMES=$(echo "$PACKAGE_DIRS" | sed 's/packages\///g')
-if [ -z "$PACKAGE_NAMES" ]; then
-	echo "[*] No modified packages found." >&2
-	exit 0
-fi
+## Filter excluded packages.
+for pkg in $EXCLUDED_PACKAGES; do
+	PACKAGE_NAMES=$(sed -E "s/(^|\s\s*)${pkg}(\$|\s\s*)/ /g" <<< "$PACKAGE_NAMES")
+done
+unset pkg
+
+## Remove trailing spaces.
+PACKAGE_NAMES=$(sed 's/[[:blank:]]*$//' <<< "$PACKAGE_NAMES")
 
 set -e
 
 ###############################################################################
 ##
-##  Building packages.
+##  Executing requested actions. Only one per script session.
 ##
 ###############################################################################
 
-if ! $DO_UPLOAD; then
-	echo "[*] Building packages: $PACKAGE_NAMES"
-	echo
-	if [ -n "$CIRRUS_PR" ]; then
-		echo "    Pull request: https://github.com/termux/termux-root-packages/pull/${CIRRUS_PR}"
-	else
-		if [ -n "$CIRRUS_LAST_GREEN_CHANGE" ]; then
-			echo "    Changes: ${CIRRUS_LAST_GREEN_CHANGE}..${CIRRUS_CHANGE_IN_REPO}"
-		else
-			echo "    Changes: ${CIRRUS_CHANGE_IN_REPO}"
-		fi
-	fi
+case "$1" in
+	--upload)
+		if [ -n "$PACKAGE_NAMES" ]; then
 
-	cd "${REPO_DIR}/${BUILD_ENVIRONMENT}" || {
-		echo "[!] Failed to cd into '${REPO_DIR}/${BUILD_ENVIRONMENT}'."
-		exit 1
-	}
-
-	echo
-	for pkg in $PACKAGE_NAMES; do
-		./build-package.sh -a "$TERMUX_ARCH" -I "$pkg"
-	done
-	echo
-fi
-
-###############################################################################
-##
-##  Storing packages in cache.
-##
-###############################################################################
-
-if [ "$CIRRUS_BRANCH" = "master" ]; then
-	if ! $DO_UPLOAD; then
-		ARCHIVE_NAME="debs-${TERMUX_ARCH}-${CIRRUS_CHANGE_IN_REPO}.tar.gz"
-
-		if [ -d "${REPO_DIR}/${BUILD_ENVIRONMENT}/debs" ]; then
-			echo "[*] Archiving packages into '${ARCHIVE_NAME}'."
-			tar zcf "$ARCHIVE_NAME" debs
-
-			echo "[*] Uploading '${ARCHIVE_NAME}' to cache:"
-			echo
-			curl --upload-file "$ARCHIVE_NAME" \
-				"http://$CIRRUS_HTTP_CACHE_HOST/${ARCHIVE_NAME}"
-			echo
-		fi
-	else
-		for arch in aarch64 arm i686 x86_64; do
-			ARCHIVE_NAME="debs-${arch}-${CIRRUS_CHANGE_IN_REPO}.tar.gz"
-
-			echo "[*] Downloading '$ARCHIVE_NAME' from cache:"
-			echo
-			curl --output "/tmp/${ARCHIVE_NAME}" \
-				"http://$CIRRUS_HTTP_CACHE_HOST/${ARCHIVE_NAME}"
-			echo
-
-			if [ -s "/tmp/${ARCHIVE_NAME}" ]; then
-				echo "[*] Unpacking '/tmp/${ARCHIVE_NAME}':"
-				echo
-				tar xvf "/tmp/${ARCHIVE_NAME}"
-				echo
-			else
-				echo "[!] Empty archive '/tmp/${ARCHIVE_NAME}'."
+			if [ -z "$BINTRAY_API_KEY" ]; then
+				echo "[!] Can't upload packages without Bintray API key."
+				exit 1
 			fi
-		done
 
-		echo "[*] Uploading packages to Bintray:"
-		echo
-		"${REPO_DIR}/scripts/package_uploader.sh" -p "${PWD}/debs" $PACKAGE_NAMES
-	fi
-fi
+			if [ -z "$BINTRAY_GPG_PASSPHRASE" ]; then
+				echo "[!] Can't upload packages without GPG passphrase."
+				exit 1
+			fi
+
+			echo "[*] Uploading packages to Bintray:"
+			"${REPO_DIR}/scripts/package_uploader.sh" -p "${PWD}/debs" $PACKAGE_NAMES
+		else
+			echo "[*] No modified packages found."
+			exit 0
+		fi
+		;;
+	*)
+		if [ -n "$PACKAGE_NAMES" ]; then
+			echo "[*] Building packages:" $PACKAGE_NAMES
+			./build-package.sh -a "$TERMUX_ARCH" -I $PACKAGE_NAMES
+		else
+			echo "[*] No modified packages found."
+			exit 0
+		fi
+		;;
+esac
