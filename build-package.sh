0e8adbdbc22c7e28175c0c64be3176e39cf82c8a
Henrik Grimler <henrik@grimler.se>
2019-10-24 11:31:37 +0200

    libdevmapper: remove faulty API_LEVEL target

--- build-package.sh
+++ build-package.sh
@@ -2,11 +2,5 @@
 
 set -e -o pipefail -u
 
-# link into main package repo to handle dependencies
-for package in packages/*; do
-    #TODO: handle docker image builds better
-    ln -sf ../../${package} termux-packages/packages/
-done
-
 cd termux-packages
 ./build-package.sh "$@"
