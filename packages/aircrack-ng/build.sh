f83117a36c8e50fbda82b9f9061177fe028e60c4
Henrik Grimler <henrik@grimler.se>
2020-02-29 21:31:47 +0100

    aircrack: update to 1.6 and blacklist aarch64

--- packages/aircrack-ng/build.sh
+++ packages/aircrack-ng/build.sh
@@ -1,13 +1,10 @@
-TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
-TERMUX_PKG_HOMEPAGE=https://www.aircrack-ng.org/
+TERMUX_PKG_HOMEPAGE=https://www.aircrack-ng.org
 TERMUX_PKG_DESCRIPTION="WiFi security auditing tools suite"
 TERMUX_PKG_LICENSE="GPL-2.0"
-#1.5.1 (requires autoconf, automake, libtool, shtool, pkg-config (and maybe libstdc and make/gmake) support)
-TERMUX_PKG_VERSION=1.2-rc4
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://github.com/aircrack-ng/aircrack-ng/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ed241ea423cab1c86e7882e901d5200d91b2f97d7efd703b0acf17742be47f9b
-TERMUX_PKG_DEPENDS="libc++, libnl, openssl, libpcap, pciutils"
-TERMUX_PKG_BUILD_IN_SRC=true
-
-# TODO: in termux-packages, add support for these packages: shtool, libstdc, gmake
+TERMUX_PKG_VERSION=1.6
+TERMUX_PKG_SRCURL=https://download.aircrack-ng.org/aircrack-ng-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4f0bfd486efc6ea7229f7fbc54340ff8b2094a0d73e9f617e0a39f878999a247
+TERMUX_PKG_DEPENDS="libnl, openssl, libpcap, pciutils"
+# clang can't handle ternary instrutions on aarch64:
+# https://github.com/aircrack-ng/aircrack-ng/issues/1957
+TERMUX_PKG_BLACKLISTED_ARCHES="aarch64"
