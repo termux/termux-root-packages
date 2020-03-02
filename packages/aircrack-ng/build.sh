355bd0830bc86a66ce8a59e10c8c6f21ee508e11
Henrik Grimler <henrik@grimler.se>
2020-03-02 11:59:31 +0100

    aircrack: set ax_cv_neon_cflags=no
    Compiling on device on arm sets this, while cross compiling for arm
    sets it to yes.
    
    Hopefully this will also make it possible to compile aircrack for
    aarch64 as well.

--- packages/aircrack-ng/build.sh
+++ packages/aircrack-ng/build.sh
@@ -5,9 +5,7 @@ TERMUX_PKG_VERSION=1.6
 TERMUX_PKG_SRCURL=https://download.aircrack-ng.org/aircrack-ng-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4f0bfd486efc6ea7229f7fbc54340ff8b2094a0d73e9f617e0a39f878999a247
 TERMUX_PKG_DEPENDS="libnl, openssl, libpcap, pciutils"
-# clang can't handle ternary instrutions on aarch64:
-# https://github.com/aircrack-ng/aircrack-ng/issues/1957
-TERMUX_PKG_BLACKLISTED_ARCHES="aarch64"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ax_cv_neon_cflags=no"
 
 termux_step_pre_configure() {
 	NOCONFIGURE=1 ./autogen.sh
