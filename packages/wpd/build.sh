a76b91d5cfe9048af9b8755d02f0d820b036a3e5
Kleo Bercero <kbeflo@gmail.com>
2020-08-09 12:37:25 +0800

    Update build.sh

--- packages/wpd/build.sh
+++ packages/wpd/build.sh
@@ -5,21 +5,17 @@ TERMUX_PKG_MAINTAINER="Kleo Bercero @kbeflo"
 TERMUX_PKG_VERSION=1.3.2
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_SRCURL=https://github.com/kbeflo/wpd/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=37ce0707cbe46acb532d8b07fa958f160675c6e3250360a8c40413b9216e2eaf
 
 termux_step_extract_package() {
-	local CHECKED_OUT_FOLDER=$TERMUX_PKG_CACHEDIR/wpd-checkout-$TERMUX_PKG_VERSION
 	if [ ! -d $CHECKED_OUT_FOLDER ]; then
 		local TMP_CHECKOUT=$TERMUX_PKG_TMPDIR/tmp-checkout
 		rm -Rf $TMP_CHECKOUT
 		mkdir -p $TMP_CHECKOUT
 
-		git clone --depth 1 \
-			--branch master \
-			https://github.com/kbeflo/wpd.git \
-			$TMP_CHECKOUT
 		cd $TMP_CHECKOUT
-		git fetch --all --tags --prune
-		git checkout "tags/$TERMUX_PKG_VERSION" -b "$TERMUX_PKG_VERSION"
+		tar -xf ${TERMUX_PKG_VERSION}.tar.gz wpd
 		mv $TMP_CHECKOUT $CHECKED_OUT_FOLDER
 	fi
 
