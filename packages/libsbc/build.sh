40a17e60b52a58be1cf4a7d4cc0bbae41068d691
Henrik Grimler <henrik@grimler.se>
2020-10-01 21:49:39 +0200

    libsbc: add package

---
+++ packages/libsbc/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=http://bluez.org
+TERMUX_PKG_DESCRIPTION="Digital audio encoder and decoder used to transfer data to Bluetooth audio output devices"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.4
+TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/bluetooth/sbc-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=518bf46e6bb3dc808a95e1eabad26fdebe8a099c1e781c27ed7fca6c2f4a54c9
