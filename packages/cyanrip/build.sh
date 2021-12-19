e19d7d4685fa305d92d8a726433a646361a4bf1f
tathastu871 <daichiash1997@gmail.com>
2021-12-19 15:57:58 +0530

    Added cyanrip and dependencies

--- packages/cyanrip/build.sh
+++ packages/cyanrip/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/cyanreg/cyanrip
-TERMUX_PKG_DESCRIPTION="Fully featured CD ripping program able to take out most of the tedium. Fully accurate, has advanced features most rippers don't, yet has no bloat and is cross-platform."
+TERMUX_PKG_DESCRIPTION="Fully featured CD ripping program."
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_LICENSE="LGPL- 2.1"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=0.7.0
 TERMUX_PKG_SRCURL=https://github.com/cyanreg/cyanrip/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=67318dee2a21ed17b98b406dfd2c86568c649fc4596c8fb22f8a767b7a7406d0
--- packages/libcdio-paranoia/build.sh
+++ packages/libcdio-paranoia/build.sh
@@ -1,20 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libcdio
 TERMUX_PKG_DESCRIPTION="This is a port of xiph.org's cdda paranoia to use libcdio for CDROM access."
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_LICENSE="GNU Public License 3.0"
+TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=10.2+2.0.1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libcdio/libcdio-paranoia-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=28d7d00e4a83d0221acda0fd2eb3e3240bf094db4c00a85998922201939fa952
-TERMUX_PKG_DEPENDS="libcdio"
-TERMUX_INSTALL_DEPS=true
-TERMUX_PKG_BUILD_IN_SRC=true
-termux_step_configure() {
-	${TERMUX_PKG_SRCDIR}/configure --prefix=${PREFIX} LDFLAGS=" -landroid-glob"
+TERMUX_PKG_DEPENDS="libcdio, libandroid-glob"
+termux_step_pre_configure() {
+	LDFLAGS=" -landroid-glob"
 }
-termux_step_make(){
-	cd ${TERMUX_PKG_SRCDIR} && make -j8
-}
-termux_step_make_install() {
-	cd ${TERMUX_PKG_SRCDIR} && make install
-}
-
